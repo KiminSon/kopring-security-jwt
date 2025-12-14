02883de44d1f113dc3a28e5ac5f99c91bcb724e5
KiminSon <123408986+KiminSon@users.noreply.github.com>
2025-12-14 18:04:49 +0900

    Initial commit

---
+++ src/main/kotlin/com/practice/kopringsecurityjwt/KopringSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.kopringsecurityjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringSecurityJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringSecurityJwtApplication>(*args)
+}
